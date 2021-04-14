e56c54970a33822e1785ab67cc8a1972fddbb9e5
cmmishra9 <chandanmshr476@gmail.com>
2021-04-14 11:48:06 +0530

    Birthday reminder project

---
+++ script.js
@@ -0,0 +1,38 @@
+
+// window.onload=function(){
+    const comingYear = document.querySelector(".coming-year");
+    const birthDate = prompt("Enter birth date (1-31): ");
+    const birthMonth = prompt("Enter birth month (1-12): ");
+
+    // date object
+
+    const today = new Date();
+
+    let nextYear;
+
+    if(today.getMonth() + 1> birthMonth || today.getMonth()+1 == birthMonth && today.getDate()+1> birthDate){
+       nextYear = new Date().getFullYear() +1; 
+    }else{
+        nextYear = new Date().getFullYear();
+    }
+
+    comingYear.innerHTML = nextYear;
+
+    function countdown(){
+        const remaining = new Date(`${nextYear}-${birthMonth}-${birthDate} 00:00:00`)-new Date();
+        // console.log(remaining);
+        const days = Math.floor(remaining / (24*60*60*1000));
+        const hours = Math.floor(remaining / (60*60*1000))%24;
+        const minutes = Math.floor(remaining / (60*1000))%60;
+        const seconds = Math.floor(remaining/1000)%60;
+
+        document.querySelector(".days").innerHTML = days;
+        document.querySelector(".hours").innerHTML = hours;
+        document.querySelector(".minutes").innerHTML = minutes;
+        document.querySelector(".seconds").innerHTML = seconds;
+    }
+// }
+
+  countdown()
+
+  setInterval(countdown, 1000);
